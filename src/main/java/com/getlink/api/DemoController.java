74099460c1490a065822834c964bae8bdcd3fdb0
Tran Dang Tin <thantienbut@gmail.com>
2018-12-09 21:58:27 -0800

    add new

--- src/main/java/com/getlink/api/DemoController.java
+++ src/main/java/com/getlink/api/DemoController.java
@@ -16,11 +16,11 @@ import com.getlink.constants.Constants;
 @RequestMapping(value = "/api")
 public class DemoController {
 
-	@RequestMapping(value = "/demo", method = RequestMethod.GET)
+	@RequestMapping(value = "/anhtindeptrai", method = RequestMethod.GET)
 	public ResponseEntity<?> getDemo(@RequestParam String linkFshare) throws JSONException {
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("statusCode", Constants.STATUS_OK);
-		jsonObject.put("demo", "demo nè");
+		jsonObject.put("anhtindeptrai", "anhtindeptrai");
 		ResponseEntity<String> retult = new ResponseEntity<String>(jsonObject.toString(), new HttpHeaders(),
 				HttpStatus.OK);
 		return retult;
