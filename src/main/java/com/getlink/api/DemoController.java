5b5f2de8a16e62311e29114fd63e481472a74c99
Tran Dang Tin <thantienbut@gmail.com>
2018-12-09 22:51:26 -0800

    update

--- src/main/java/com/getlink/api/DemoController.java
+++ src/main/java/com/getlink/api/DemoController.java
@@ -5,9 +5,9 @@ import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.getlink.constants.Constants;
@@ -17,6 +17,7 @@ import com.getlink.constants.Constants;
 public class DemoController {
 
 	@RequestMapping(value = "/anhtindeptrai", method = RequestMethod.GET)
+	@CrossOrigin(origins = "http://localhost:80")
 	public ResponseEntity<?> getDemo() throws JSONException {
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("statusCode", Constants.STATUS_OK);
